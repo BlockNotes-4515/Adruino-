c35879618e7fa826caa293fcde985fecdd0b1efb
Dhruv Dhayal <137479629+BlockNotes-4515@users.noreply.github.com>
2024-03-10 14:27:04 +0530

    Variables & Datatypes. ✔

---
+++ Variables___datatypes.ino/Variables___datatypes.ino.ino
@@ -0,0 +1,33 @@
+//-------------------------------- Variables and Datatypes in Adruino Projects! ------------------------------------
+
+/*========================== Varibales! ===============================
+
+1. Name of the Particular Variables.
+2. Each Variables should have their own Datatype.
+3. Variable just as like a Container to store values by assignation (=) Assignment Operator.
+ */
+
+//cout<<"\n These are the Various Examples to show!
+
+ int a=90;
+ float b=89.8;
+ String name="Dhruv Dhayal";
+
+ //----------------------------- Types of Variables! ---------------------------
+
+/* 1. Local Variable: Declared inside the function and can be acessed inside the function only not outside the functions.
+ 2. Global Variables: Dclared outside the Functions and can be acessed anywhere inside / outside the function both.
+*/ 
+int d;      //Global Variables.
+ void setup()
+ {
+  // Main Program concept part1;
+  int var1;                             //Local Variables.
+  int n;
+/*  cout<<"\n 1. Enter the Values of n: ";
+  cin>>n;*/
+ }
+ void loop()
+ {
+  // Now, We have to write the code in the given loop.
+ }
