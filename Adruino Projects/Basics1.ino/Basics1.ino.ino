94cb25c48d470aab96209a76f61b02e90e77e366
Dhruv Dhayal <137479629+BlockNotes-4515@users.noreply.github.com>
2024-03-10 14:11:31 +0530

    Uploaded Adruino Basics Files. ✔

---
+++ Adruino Projects/Basics1.ino/Basics1.ino.ino
@@ -0,0 +1,22 @@
+//Adruino has two main Basic Fundamental Conponents!
+
+//======================= Adruino Basics Fundamentals! ================================
+
+/*   1. Similar to Other Programming Languages!
+ *   2. Use Terminator (;) semi-colon after every statement.
+ *   3. Two Comments allowed (i). single-line (//) & Multi-Line (/*).
+ *   4. All program or the Function is Case-Sensitive.
+ *   5. They Should Enclose Under {} Curly Braces.
+ *   6. Each Adruino file can be ended with extension is (.ino)/;
+ */
+
+ //---------------------- Examples! -----------------------
+
+ void setup()
+ {
+    //Write the Code-Statement 1;
+ }
+void loop()
+{
+    //Write the Loop any code inside the code is part of loop;
+}
